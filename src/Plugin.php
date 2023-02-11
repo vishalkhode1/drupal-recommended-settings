ffe7463c7efba59fa89be3f948e3b72bae9d00be
Vishal Khode <vishal.khode@acquia.com>
2023-02-11 13:50:25 +0530

    ACMS-000: Initial codebase added.

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,18 +3,16 @@
 namespace Acquia\Drupal\RecommendedSettings;
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\OperationInterface;
+use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\IO\IOInterface;
 use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
-use Composer\Plugin\Capability\CommandProvider;
-use Composer\Plugin\Capable;
-use Composer\Plugin\CommandEvent;
-use Composer\Plugin\PluginEvents;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
-use Drupal\Composer\Plugin\Scaffold\CommandProvider as ScaffoldCommandProvider;
 
 /**
  * Composer plugin for handling drupal scaffold.
@@ -23,129 +21,137 @@ use Drupal\Composer\Plugin\Scaffold\CommandProvider as ScaffoldCommandProvider;
  */
 class Plugin implements PluginInterface, EventSubscriberInterface {
 
-    /**
-     * The Composer service.
-     *
-     * @var \Composer\Composer
-     */
-    protected $composer;
+  /**
+   * The Composer service.
+   *
+   * @var \Composer\Composer
+   */
+  protected $composer;
 
-    /**
-     * Composer's I/O service.
-     *
-     * @var \Composer\IO\IOInterface
-     */
-    protected $io;
+  /**
+   * Composer's I/O service.
+   *
+   * @var \Composer\IO\IOInterface
+   */
+  protected $io;
 
-    /**
-     * The Composer Scaffold handler.
-     *
-     * @var \Drupal\Composer\Plugin\Scaffold\Handler
-     */
-    protected $handler;
+  /**
+   * The Composer Scaffold handler.
+   *
+   * @var \Drupal\Composer\Plugin\Scaffold\Handler
+   */
+  protected $handler;
 
-    /**
-     * Record whether the 'require' command was called.
-     *
-     * @param bool
-     */
-    protected $requireWasCalled;
+  /**
+   * Record whether the 'require' command was called.
+   *
+   * @var bool
+   */
+  protected $bltIncluded;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function activate(Composer $composer, IOInterface $io) {
-        $this->composer = $composer;
-        $this->io = $io;
-        $this->requireWasCalled = FALSE;
+  /**
+   * {@inheritdoc}
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    $this->composer = $composer;
+    $this->io = $io;
+    $this->bltIncluded = FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deactivate(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function uninstall(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      PackageEvents::POST_PACKAGE_INSTALL => "onPostPackageEvent",
+      PackageEvents::POST_PACKAGE_UPDATE => "onPostPackageEvent",
+      ScriptEvents::POST_UPDATE_CMD => "onPostCmdEvent",
+      ScriptEvents::POST_INSTALL_CMD => "onPostCmdEvent",
+    ];
+  }
+
+  /**
+   * Marks blt to be processed after an install or update command.
+   *
+   * @param \Composer\Installer\PackageEvent $event
+   *   Event.
+   */
+  public function onPostPackageEvent(PackageEvent $event) {
+    $package = $this->getSettingsPackage($event->getOperation());
+    if ($package) {
+      // By explicitly setting the blt package, the onPostCmdEvent() will
+      // process the update automatically.
+      $this->settingsPackage = $package;
     }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function deactivate(Composer $composer, IOInterface $io) {
+  /**
+   * Execute blt blt:update after update command has been executed.
+   *
+   * @throws \Exception
+   */
+  public function onPostCmdEvent() {
+    // Only install the template files if acquia/blt was installed.
+    if (isset($this->settingsPackage)) {
+      $settings = new Settings($this->composer, $this->io, $this->settingsPackage);
+      $settings->hashSalt();
+      $settings->generateSettings();
     }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function uninstall(Composer $composer, IOInterface $io) {
+  /**
+   * Gets the acquia/drupal-recommended-settings package.
+   *
+   * @param \Composer\DependencyResolver\Operation\OperationInterface $operation
+   *   Op.
+   *
+   * @return mixed|null
+   *   Returns mixed or NULL.
+   */
+  protected function getSettingsPackage(OperationInterface $operation) {
+    if ($operation instanceof InstallOperation) {
+      $package = $operation->getPackage();
     }
-
-    /**
-     * {@inheritdoc}
-     */
-//    public function getCapabilities() {
-//        return [CommandProvider::class => ScaffoldCommandProvider::class];
-//    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents() {
-        // Important note: We only instantiate our handler on "post" events.
-        return [
-            Handler::POST_DRUPAL_SCAFFOLD_CMD => "postDrupalScaffold",
-//            ScriptEvents::POST_UPDATE_CMD => 'postCmd',
-//            ScriptEvents::POST_INSTALL_CMD => 'postCmd',
-//            PackageEvents::POST_PACKAGE_INSTALL => 'postPackage',
-//            PluginEvents::COMMAND => 'onCommand',
-        ];
+    elseif ($operation instanceof UpdateOperation) {
+      $package = $operation->getTargetPackage();
     }
-
-    public function postDrupalScaffold(Event $event) {
-        $this->io->write("Post Drupal scaffold command invoked.");
+    if (isset($package) && $package instanceof PackageInterface && $package->getName() == "acquia/drupal-recommended-settings") {
+      return $package;
     }
-//    /**
-//     * Post command event callback.
-//     *
-//     * @param \Composer\Script\Event $event
-//     *   The Composer event.
-//     */
-//    public function postCmd(Event $event) {
-//        $this->io->write("Post update");
-//    }
-//
-//    /**
-//     * Post package event behavior.
-//     *
-//     * @param \Composer\Installer\PackageEvent $event
-//     *   Composer package event sent on install/update/remove.
-//     */
-//    public function postPackage(PackageEvent $event) {
-//        $this->handler()->onPostPackageEvent($event);
-//    }
-//
-//    /**
-//     * Pre command event callback.
-//     *
-//     * @param \Composer\Plugin\CommandEvent $event
-//     *   The Composer command event.
-//     */
-//    public function onCommand(CommandEvent $event) {
-//        if ($event->getCommandName() == 'require') {
-//            if ($this->handler) {
-//                throw new \Error('Core Scaffold Plugin handler instantiated too early. See https://www.drupal.org/project/drupal/issues/3104922');
-//            }
-//            $this->requireWasCalled = TRUE;
-//        }
-//    }
-//
-//    /**
-//     * Lazy-instantiate the handler object. It is dangerous to update a Composer
-//     * plugin if it loads any classes prior to the `composer update` operation,
-//     * and later tries to use them in a post-update hook.
-//     */
-//    protected function handler() {
-//        if (!$this->handler) {
-//            $this->handler = new Handler($this->composer, $this->io);
-//            // On instantiation of our handler, notify it if the 'require' command
-//            // was executed.
-//            if ($this->requireWasCalled) {
-//                $this->handler->requireWasCalled();
-//            }
-//        }
-//        return $this->handler;
-//    }
+    return NULL;
+  }
+
+  /**
+   * Hook for pre-package install.
+   */
+  public function prePackageInstall(PackageEvent $event) {
+    if (!$this->bltIncluded) {
+      $operations = $event->getOperations();
+      foreach ($operations as $operation) {
+        if ($operation instanceof InstallOperation) {
+          $package = $operation->getPackage();
+        }
+        elseif ($operation instanceof UpdateOperation) {
+          $package = $operation->getTargetPackage();
+        }
+        if (isset($package) && $package instanceof PackageInterface && $package->getName() == 'acquia/blt') {
+          $this->bltIncluded = TRUE;
+          break;
+        }
+      }
+    }
+  }
 
 }
